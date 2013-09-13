376c8de02e16e8d4a53d6803b400b8b4fd823773
Fabio Quarantini <wlospam@gmail.com>
2013-09-13 16:35:27 +0200

    Added main files

---
+++ js/main.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+
+	var $response = $("<div/>");
+	
+	$("button").on("click", function(e) {
+
+		var responseClasses = "";
+		var $form = $("#newsletter-form");
+		var serializedData = $form.serialize();
+		
+		/* Add form action http://kb.mailchimp.com/article/can-i-host-my-own-sign-up-forms. Change "post?" to "post-json?" and add "&c=?" at the end */
+		var actionUrl = 'http://fabioquarantini.us7.list-manage2.com/subscribe/post-json?u=679769a7357f9ee98247a003d&amp;id=cdb15a7d9d&c=?';
+
+		$.post( actionUrl , serializedData, function(data) {
+			responseClasses = "mc-" + data.result + " mc-response";
+			$response.removeClass().addClass(responseClasses).text("").text(data.msg);
+			$response.appendTo( $form );
+		}, "jsonp");
+
+		e.preventDefault();
+	});
+
+});
