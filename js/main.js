db90c9b9c20235c07af25a8f688e961e9583995f
Fabio Quarantini <wlospam@gmail.com>
2013-09-16 09:45:13 +0200

    Optimized scripts

--- js/main.js
+++ js/main.js
@@ -11,10 +11,13 @@ $(document).ready(function() {
 		/* Add form action http://kb.mailchimp.com/article/can-i-host-my-own-sign-up-forms. Change "post?" to "post-json?" and add "&c=?" at the end */
 		var actionUrl = 'http://fabioquarantini.us7.list-manage2.com/subscribe/post-json?u=679769a7357f9ee98247a003d&amp;id=cdb15a7d9d&c=?';
 
+		$form.find('button').addClass('button-loading');
+
 		$.post( actionUrl , serializedData, function(data) {
 			responseClasses = "mc-" + data.result + " mc-response";
 			$response.removeClass().addClass(responseClasses).text("").text(data.msg);
 			$response.appendTo( $form );
+			$form.find('button').removeClass();
 		}, "jsonp");
 
 		e.preventDefault();
