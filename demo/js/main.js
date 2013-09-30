abcd60227ecbca9581a25ca7be5660e162635d06
Fabio Quarantini <wlospam@gmail.com>
2013-09-30 16:05:01 +0200

    Added demo page

---
+++ demo/js/main.js
@@ -0,0 +1,12 @@
+$(document).ready( function() {
+
+	$('form').formchimp({
+		//'appendElement' : "h1",
+		'buttonText' : 'Thanks',
+		//'errorMessage' : "Error message",
+		//'responseClass' : "mc-response",
+		//'successMessage' : "Send message",
+		//'url': actionUrl,
+	});
+
+});
